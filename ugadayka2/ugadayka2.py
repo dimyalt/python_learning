b28a6affcfc34b0054cf969068733c0eb24de7ff
dimyalt <79787789167@yandex.ru>
2021-05-25 11:26:51 +0300

    Added a character-cheking function

--- ugadayka2/ugadayka2.py
+++ ugadayka2/ugadayka2.py
@@ -2,5 +2,14 @@ from random import *
 x = randint(1, 100)
 print(x)
 print('Добро пожаловать в числовую угадайку')
-y = int(input())
- 
+y = input()
+
+def is_valid(character):
+    if character.isdigit() == True and 0 < int(character) < 101 :
+        print('True')
+        return True
+    else:
+        print('false')
+        return False        
+
+is_valid(y)    
