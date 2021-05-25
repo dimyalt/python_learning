7e26dfd30d11f5a4f716a54934e46ef7a2b64308
dimyalt <79787789167@yandex.ru>
2021-05-25 12:36:35 +0300

    Added a loop to check the entered values

--- ugadayka2/ugadayka2.py
+++ ugadayka2/ugadayka2.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 from random import *
+
 x = randint(1, 100)
-print(x)
 print('Добро пожаловать в числовую угадайку')
-y = input()
 
 def is_valid(character):
     if character.isdigit() == True and 0 < int(character) < 101 :
-        print('True')
         return True
     else:
-        print('false')
-        return False        
+        return False    
 
-is_valid(y)    
+while True:   
+    y = input('Введите число: ')
+    while is_valid(y) != True:   
+        y = input('А может быть все-таки введем целое число от 1 до 100?\n')
+    else:
+        y = int(y)
+    
+    if x < y:
+        print('Слишком много, попробуйте еще раз!')
+        continue
+    elif x > y:
+        print('Слишком мало, попробуйте еще раз!')
+        continue
+    else:    
+        print('Вы угадали, поздравляем!')
+        break  
