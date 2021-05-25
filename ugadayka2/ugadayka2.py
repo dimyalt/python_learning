03521619ef94b8891ee38dadbbe5f0b5ea57db07
dimyalt <79787789167@yandex.ru>
2021-05-25 13:26:44 +0300

    added a counter for attempts

--- ugadayka2/ugadayka2.py
+++ ugadayka2/ugadayka2.py
@@ -8,22 +8,27 @@ def is_valid(character):
     if character.isdigit() == True and 0 < int(character) < 101 :
         return True
     else:
-        return False    
-
+        return False 
+    
+    
+counter = 0
 while True:   
     y = input('Введите число: ')
     while is_valid(y) != True:   
         y = input('А может быть все-таки введем целое число от 1 до 100?\n')
     else:
         y = int(y)
-    
+
     if x < y:
         print('Слишком много, попробуйте еще раз!')
+        counter += 1
         continue
     elif x > y:
         print('Слишком мало, попробуйте еще раз!')
+        counter += 1
         continue
     else:    
+        counter += 1
         print('Вы угадали, поздравляем!')
-        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        print(f'Спасибо, что играли в числовую угадайку. Было: {counter} попыток. Еще увидимся...')
         break  
