4dc815befc61c270aaa77f3ed2052bf8e37d2e82
dimyalt <79787789167@yandex.ru>
2021-05-26 17:57:45 +0300

    the basic functionality of the password generator was created

--- secure_password/README.md
+++ secure_password/README.md
@@ -1,4 +1,4 @@
-# secure password programm / програмаа генерации безопасных паролей
+# secure password programm / программа генерации безопасных паролей
 Project description: The program generates a preset number of passwords and includes smart settings for the length of the password, as well as which characters should be included and which should be excluded.
 
 
--- secure_password/secure_password.py
+++ secure_password/secure_password.py
@@ -1,1 +1,33 @@
-# -*- coding: utf-8 -*-
+from random import choice
+
+print('Добро пожаловать в генератор паролей v.1.0')
+
+pass_leight = int(input('Введите длину пароля: '))
+print(f'Длина пароля {pass_leight} символов.')
+numbers = input('Нужны ли цифры? [да/нет] ').lower() == 'да'
+letters = input('Нужны ли буквы? [да/нет] ').lower() == 'да'
+specials = input('Нужны ли знаки? [да/нет] ').lower() == 'да'
+summary = []
+print(numbers, 'numbers')
+
+def creating_password(numb, lett, spec):
+    numbers_list = list(range(48, 58))
+    letters_list = list(range(65, 91))
+    letters_list_2 = list(range(97, 123))
+    specials_list = list(range(33, 48))
+    specials_list_2 = list(range(91, 96))
+    result_list = []
+
+    if numb:
+        result_list.extend(numbers_list)
+    if lett:
+        result_list.extend(letters_list)
+        result_list.extend(letters_list_2)
+    if spec:
+        result_list.extend(specials_list)
+        result_list.extend(specials_list_2)
+    return choice(result_list)
+
+for _ in range(pass_leight):
+    summary.append(chr(creating_password(numbers, letters, specials)))
+print('Ваш пароль: ', *summary, sep='')
