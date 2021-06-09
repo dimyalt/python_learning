2da75f25913f89d95115037208a1d0e1bfb4b9e3
dimyalt <79787789167@yandex.ru>
2021-06-09 17:44:08 +0300

    Minor corrections have been made

--- kogda_igra_telegram_bot/bot.py
+++ kogda_igra_telegram_bot/bot.py
@@ -2,31 +2,38 @@ from openpyxl import *
 import time
 import telebot
 
-file = load_workbook("sample.xlsx") # Открываем рабочую книгу / Opening the workbook
-list_ = file.active # Создали переменную для активного листа / Create a variable for the active sheet
+file = load_workbook("sample.xlsx")  # Открываем рабочую книгу / Opening the workbook
+list_ = file.active  # Создали переменную для активного листа / Create a variable for the active sheet
 data = []
 start_date = ''
 end_date = ''
-get_name = ''
+get_name_game = ''
 get_org_name = ''
 get_name_user = ''
 
-bot = telebot.TeleBot('1638491301:AAHqxXK9Iem9rC2fSz5mPGv_hWmJrrTnaFk') # Подключили токен телеграмм бота / We connected the bot's telegram token
+bot = telebot.TeleBot(
+    '1638491301:AAHqxXK9Iem9rC2fSz5mPGv_hWmJrrTnaFk')  # Подключили токен телеграмм бота / We connected the bot's telegram token
 
-@bot.message_handler(commands=['start']) # Обработка команды "start" / Processing the "start" command
+
+@bot.message_handler(commands=['start'])  # Обработка команды "start" / Processing the "start" command
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start') # Ответ бота на команду "start" / Bot response to "start" command
+    bot.send_message(message.chat.id,
+                     'Привет, ты написал мне /start')  # Ответ бота на команду "start" / Bot response to "start" command
 
-@bot.message_handler(commands=['help']) # Обработка команды "help" / Processing the "help" command
+
+@bot.message_handler(commands=['help'])  # Обработка команды "help" / Processing the "help" command
 def help_message(message):
     bot.send_message(message.chat.id, 'Написал мне /help - держи краткую справку:\nНапиши "игра" - получишь информацию '
                                       'про ближайшее мероприятие.\nНапиши "игры" - расскажу о ближайших 5 мероприятиях.\n'
                                       'Хочешь рассказать об игре - так и пиши - "добавить".\nРедактировать добавленную тобой '
-                                      'информацию можно, написав "перенос"') # Ответ бота на команду "help" / Bot response to "help" command
+                                      'информацию можно, написав "перенос"')  # Ответ бота на команду "help" / Bot response to "help" command
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'добавить':
+        global data;
+        data = []
         bot.send_message(message.from_user.id, "Ok, давай добавим новый движ.. \nНапиши, когда начало:");
         bot.register_next_step_handler(message, get_start_date);  # следующий шаг – функция get_start_date
     elif message.text.lower() == 'игра':
@@ -53,30 +60,33 @@ def get_end_date(message):
     end_date = message.text;
     data.append(end_date)
     bot.send_message(message.from_user.id, 'Название мероприятия?');
-    bot.register_next_step_handler(message, get_name);
+    bot.register_next_step_handler(message, get_name_game);
     print(data)
 
-def get_name(message):
-    global get_name;
-    get_name = message.text;
-    data.append(get_name)
+
+def get_name_game(message):
+    global get_name_game;
+    get_name_game = message.text;
+    data.append(get_name_game)
     bot.send_message(message.from_user.id, 'Кто организатор?');
-    bot.register_next_step_handler(message, get_org_name);
+    bot.register_next_step_handler(message, get_orgname);
     print(data)
 
-def get_org_name(message):
-    global get_org_name;
+
+def get_orgname(message):
+    global get_orgname;
     get_org_name = message.text;
     data.append(get_org_name)
     bot.send_message(message.from_user.id, 'Тип игры?');
     bot.register_next_step_handler(message, get_type_game);
     print(data)
 
+
 def get_type_game(message):
     global get_type_game;
     global get_name_user;
     get_type_game = message.text;
-    get_name_user = message.from_user.id;
+    get_name_user = message.from_user.username;
     data.append(get_type_game)
     data.append(get_name_user)
     list_.append(data)
@@ -85,5 +95,5 @@ def get_type_game(message):
     print(list_, "list_", data, "data")
 
 
-
-bot.polling(none_stop=True, interval=0) # Опрос телеграмма на входящие сообщения / Telegram survey for incoming messages
+bot.polling(none_stop=True,
+            interval=0)  # Опрос телеграмма на входящие сообщения / Telegram survey for incoming messages
